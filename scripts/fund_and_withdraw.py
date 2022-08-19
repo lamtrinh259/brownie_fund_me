c9b725cb4329fadc1f9031a919178d04ccb68a67
Lam <lam.trinh@global.t-bird.edu>
2022-08-19 23:09:16 +0900

    Failed the test of fund and withdraw, but everything else is fine

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,29 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
+    fund_me.fund(
+        {
+            "from": account,
+            "gas_limit": 12000000,
+            "allow_revert": True,
+            "value": entrance_fee,
+        }
+    )
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
+def main():
+    fund()
+    withdraw()
